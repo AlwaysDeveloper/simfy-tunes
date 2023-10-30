9ab18da542f8c622c16ab8ad46efd5c6af9e950c
Mukul Gupta <mukul.gupta01@nagarro.com>
2023-10-30 14:01:23 +0530

    adding authentication

--- src/tracks/tracks.controller.ts
+++ src/tracks/tracks.controller.ts
@@ -1,14 +1,16 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { TracksService } from './tracks.service';
 import { Track } from './interfaces/track.interface';
 import { CreateTrackDto } from './dtos/createTrack.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @ApiTags('Tracks')
 @Controller('tracks')
 export class TracksController {
   constructor(private trackService: TracksService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() createTrackDto: CreateTrackDto): Promise<Track> {
     return this.trackService.create(createTrackDto);
